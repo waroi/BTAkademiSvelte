f165788a5b8d5ff1194349104d3448eaaa59e64d
Varol Maksutoglu <varolmaksutoglu@yandex.com>
2022-09-29 21:38:42 +0300

    Ders 01

---
+++ Dersler/Ders02/myapp/src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+  import Todos from "./Todos.svelte";
+  // let number = 0;
+  // const handleClick = () => (number += 1);
+  // let names = ["Başar", "Serkan", "Ece", "Tolga"];
+</script>
+
+<Todos />
+
+<!-- 
+<button on:click={handleClick}> Sayaç </button>
+
+{#if number === 10}
+  <p>Bu butona 10 kez bastınız, Tebrikler</p>
+{:else if number === 15}
+  <p>Bu butona 15 kez bastınız, Tebrikler</p>
+{:else}
+  <p>Bu butona {number} kez bastınız</p>
+{/if}
+
+<ul>
+  {#each names as name}
+    <li>{name}</li>
+  {/each}
+</ul>
+<ul>
+  {#each Array(5) as a, i}
+    <li>{i}. deneme</li>
+  {/each}
+</ul> -->
+<style>
+</style>
---
+++ Dersler/Ders02/myapp/src/Todos.svelte
@@ -0,0 +1,27 @@
+<script>
+  import TodoItem from "./TodoItem.svelte";
+  let todos = [
+    {
+      id: 1,
+      title: "todo 1",
+    },
+    {
+      id: 2,
+      title: "todo 2",
+    },
+    {
+      id: 3,
+      title: "todo 3",
+    },
+  ];
+  const clickHandle = () => {
+    todos = todos.slice(1);
+  };
+</script>
+
+<ul>
+  <button on:click={clickHandle}>İlk Elemanı Sil</button>
+  {#each todos as todo, i (todo.id)}
+    <TodoItem {todo} {i} />
+  {/each}
+</ul>
