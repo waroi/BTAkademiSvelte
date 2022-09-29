1eb1257e3fd462097d56b12d8fd7bfd10d8015b3
Tolga BALKAN <tolgabalkan94@gmail.com>
2022-09-29 21:48:11 +0300

    Update App.svelte

--- Ogrenciler/Tolga/myapp/src/App.svelte
+++ Ogrenciler/Tolga/myapp/src/App.svelte
@@ -1,20 +1,55 @@
 <script>
-  //import Result from "./Result.svelte";
-  //let number = 0;
-  //$: count = number * 2;
+  var todoItems = [
+    { content: "todo1", edit: false, status: false },
+    { content: "todo2", edit: false, status: false },
+  ];
+  let content = "";
+  function addtoDo() {
+    if (!content) return;
 
-  import Counter from "./lib/Counter.svelte";
-
-  //const handleClick =() => (number += 1);
-let image = {
-  src:  "http://localhost:5173/src/assets/svelte.svg",
-  alt: "Svelte",
-  title: "Svelte Logo",
-}
+    todoItems = [
+      ...todoItems,
+      { content: content, edit: false, status: false },
+    ];
+    content = "";
+  }
+  function deleteme(i) {
+    todoItems.splice(i, 1);
+    todoItems = todoItems;
+  }
+  function editme(i) {
+    todoItems[i].edit = !todoItems[i].edit;
+  }
 </script>
 
-<img {...image} />
+<div class="container">
+  <ul>
+    {#each todoItems as todoItem, index}
+      <li>
+        <input bind:checked={todoItem.status} type="checkbox" />
+        <input
+          class={todoItem.status ? "checked" : ""}
+          disabled={!todoItem.edit}
+          type="text"
+          value={todoItem.content}
+        />
+        <button on:click={() => editme(index)}>düzenle</button>
+        <button on:click={() => deleteme(index)}>sil</button>
+      </li>
+    {/each}
+  </ul>
+  <input type="text" bind:value={content} />
+  <button on:click={addtoDo} class="buttonGokhan">ekle</button>
+</div>
 
 <style>
-  img {height: 200px;}
+  .buttonGokhan:hover {
+    background-color: rgb(38, 119, 200);
+  }
+  .checked {
+    text-decoration: line-through;
+  }
+  .container {
+    background-image: url("https://m.media-amazon.com/images/I/517COM4H9mL._AC_SY350_.jpg");
+  }
 </style>
