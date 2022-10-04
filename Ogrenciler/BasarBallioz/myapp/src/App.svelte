14ee13e032d3c570ca2a35159745d7db70be7c6f
Başar Ballıöz <balliozbasar@gmail.com>
2022-10-04 21:07:34 +0300

    new project

--- Ogrenciler/BasarBallioz/myapp/src/App.svelte
+++ Ogrenciler/BasarBallioz/myapp/src/App.svelte
@@ -1,12 +1,5 @@
 <script>
-  import EditableText from "./EditableText.svelte";
-  import  Posts from "./Posts.svelte";
-
 </script>
- <Posts/>
-
-
 
 <style>
-
 </style>
---
+++ Ogrenciler/BasarBallioz/myapp/todoapi/myapp/src/TodoInput.svelte
@@ -0,0 +1,33 @@
+<script>
+  import request from "./Request";
+  let todo;
+
+  async function submitTodo() {
+    let todo1 = {
+      userId: 1,
+      title: todo,
+      completed: false,
+    };
+    await request("http://localhost:3000/todos", "POST", todo1);
+  }
+</script>
+
+<div class="to-do-container">
+  <input bind:value={todo} type="text" placeholder="Add your new to-do" />
+  <button on:click={submitTodo}>+</button>
+</div>
+
+<style>
+  input {
+    padding: 12px 20px;
+  }
+  button {
+    background-color: purple;
+    color: white;
+  }
+  .to-do-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
