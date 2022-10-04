c7d13121aa911b924a0a27519f7d65cc19a7fb0a
Varol Maksutoglu <varolmaksutoglu@yandex.com>
2022-10-04 21:28:02 +0300

    eventler

--- Dersler/Ders03/myapp/src/App.svelte
+++ Dersler/Ders03/myapp/src/App.svelte
@@ -1,8 +1,30 @@
 <script>
-  import AwaitBlock from "./AwaitBlock.svelte";
+  // import AwaitBlock from "./AwaitBlock.svelte";
+  // const clickHandle = (e) => console.log(e.target.parentElement.textContent);
+  import { imagePreview } from "./utils";
+
+  let image;
+  const changeHandle = async (e) => {
+    image = await imagePreview(e.target.files[0]);
+    console.log(e.target);
+  };
 </script>
 
-<AwaitBlock />
+<!-- <AwaitBlock /> -->
+<!-- <div>
+  deneme
+  <button on:click|once={clickHandle}>Tıkla</button>
+</div> -->
+<!-- <form method="get" on:submit|preventDefault={(e) => console.log(e)}>
+  <input type="text" name="name" placeholder="Ad-Soyad" /> <br />
+  <button type="submit">Gönder</button>
+</form> -->
+
+<input type="file" accept="image/*" on:change={changeHandle} />
+
+{#if image}
+  <img src={image} alt="deneme" />
+{/if}
 
 <style>
 </style>
