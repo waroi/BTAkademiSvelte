3ed844994575b73340554b30946589c1d59c4ca8
Varol Maksutoglu <varolmaksutoglu@yandex.com>
2022-10-11 18:37:05 +0300

    dispatch

---
+++ Dersler/Ders05/ders05/src/AddPerson.svelte
@@ -0,0 +1,36 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  let name, surname, salary;
+  let currency = [];
+  let send = createEventDispatcher();
+  const sendData = () => {
+    send("savedPerson", {
+      name,
+      surname,
+      salary,
+      currency,
+    });
+  };
+</script>
+
+<form on:submit|preventDefault={sendData}>
+  <div>
+    <input type="text" placeholder="name" bind:value={name} />
+  </div>
+  <div>
+    <input type="text" placeholder="surname" bind:value={surname} />
+  </div>
+  <div>
+    <input type="number" placeholder="salary" bind:value={salary} />
+  </div>
+  <div>
+    <input type="checkbox" value="TL" bind:group={currency} />
+  </div>
+  <div>
+    <input type="checkbox" value="USD" bind:group={currency} />
+  </div>
+  <div>
+    <input type="checkbox" value="BTC" bind:group={currency} />
+  </div>
+  <button type="submit">Gönder</button>
+</form>
---
+++ Dersler/Ders05/ders05/src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import AddPerson from "./AddPerson.svelte";
+
+  let user = [
+    {
+      name: "varol",
+      surname: "deneme",
+      salary: 7000,
+      currency: "",
+    },
+    {
+      name: "ad1",
+      surname: "deneme",
+      salary: 7000,
+      currency: "",
+    },
+  ];
+  const savedPerson = (e) => {
+    user = [...user, { ...e.detail }];
+  };
+</script>
+
+<div>
+  <AddPerson on:savedPerson={savedPerson} />
+  {JSON.stringify(user)}
+</div>
